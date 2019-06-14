cdc130ee32cac11de7944f4597a78e481929cc1b
Chibuike Okpaluba <cpraise@cryptoquantique.com>
2019-06-14 10:10:34 +0100

    cleared uploads

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from django.db import models
+from django.dispatch import receiver
 from django.utils import timezone
-import json
+from django.db import models
+import json, os
 
 
 class TextContent(models.Model):
@@ -19,3 +20,35 @@ class Clipboard(models.Model):
     file_content = models.ForeignKey(FileContent, on_delete=models.SET_NULL, null=True)
 
     time_stamp = models.DateTimeField(default=timezone.now, blank=False)
+
+
+@receiver(models.signals.post_delete, sender=FileContent)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+
+    '''
+        Deletes file from filesystem when corresponding Mediafile object is removed
+    '''
+
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+@receiver(models.signals.pre_save, sender=FileContent)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+
+    '''
+        Deletes old file from filesystem when corresponding Mediafile object is updated with a new file
+    '''
+
+    if not instance.pk:
+        return False
+    
+    try:
+        old_file = sender.objects.get(pk=instance.pk).file
+    except sender.DoesNotExist:
+        return False
+    
+    new_file = instance.file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
