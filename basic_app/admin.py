fc829884a2a9b7d61527be66d1b909dddfff0457
Chibuike Okpaluba <cpraise@cryptoquantique.com>
2019-06-25 15:40:12 +0100

    removed everything

--- basic_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from basic_app.models import Clipboard, TextContent, FileContent
-
-admin.site.register(Clipboard)
-admin.site.register(TextContent)
-admin.site.register(FileContent)
